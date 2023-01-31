8d228cca24ded7fd94d4071f7bf382b92d9c488c
Danil Tkachenko <dr3dfas@gmail.com>
2023-02-01 00:21:01 +0200

    vtoroeKal

--- frontend/app/src/components/StartPage/ContentStartPage/ContentStartPage.tsx
+++ frontend/app/src/components/StartPage/ContentStartPage/ContentStartPage.tsx
@@ -8,12 +8,23 @@ const ContentStartPage = () => {
         <div className={style.content}>
             <div className={style.information}>
                 <div className={style.imgsFirstPage}>
-                <p><img src = "karta.jpg" className= {style.imgFirstPage}></img></p>
-                <p><img src = "boi1.jpg" className= {style.imgFirstPage}></img></p>
-                <p><img src = "karta.jpg" className= {style.imgFirstPage}></img></p>
+                <img src = "karta.jpg" className= {style.imgFirstPage}></img>
+                <img src = "boi1.jpg" className= {style.imgFirstPage}></img>
+                <img src = "karta.jpg" className= {style.imgFirstPage}></img>
                 </div>
                 <p className={style.textFirstPage}>Salam, user. This website allows you to find any map of any building you are in.</p>
                 <p className={style.textFirstPage}>Also, in future we are planning to add some new features and possibilities here.</p>
+                {/* Второе */}
+                <div className = {style.blockSecondPage}>
+                <div className = {style.secondBlockSecondPage}>
+                <p className = {style.textSecondPage}>This project gives you such opportunities as: </p>
+                <p className = {style.textSecondPage}>- ability to check the map of the building</p>
+                <p className = {style.textSecondPage}>- ability to show the way to the point in the building</p>
+                <p className = {style.textSecondPage}>- ability to find the rooms by name or number</p>
+                </div>
+                <img src = "boi22.jpg" className={style.imgSecondPage}></img>
+                <img src = "karta2.png" className={style.imgSecondPage}></img>
+                </div>
             </div>
             {/*<div className={style.image}></div> - удалили*/}
         </div>
